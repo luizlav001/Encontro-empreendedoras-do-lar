fa99742437a4e215702351691c4895fca086362e
Gregory <gregoryguanabara@gmail.com>
2019-06-19 20:50:07 -0300

    Initial

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(window).on("scroll", function(){
+	var distanciaDoTopo = $(window).scrollTop();
+
+	if (distanciaDoTopo > 300 ) {
+		distanciaDoTopo = 300
+	}
+
+	$("#teste-scroll").css("top", (distanciaDoTopo / 10) + "px")
+
+})
+
+var opcoesSR = {
+	delay: 300,
+	duartion: 1500,
+	origin: 'bottom',
+	distance: '50px',
+	viewOffset: {
+		bottom: 240
+	}
+}
+
+ScrollReveal().reveal('#scroll-teste', opcoesSR)
+// ScrollReveal().reveal('.feature', opcoesSR)
